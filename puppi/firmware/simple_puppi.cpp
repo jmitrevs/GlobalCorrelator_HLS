f4516a2c3fb02f9564922e6e7e20a83450328d14
Jovan Mitrevski <jmitrevs@fnal.gov>
2021-06-03 13:47:45 -0500

    reg and unroll puppi

--- puppi/firmware/simple_puppi.cpp
+++ puppi/firmware/simple_puppi.cpp
@@ -54,27 +54,30 @@ void simple_puppi_hw(PFChargedObj pfch[NTRACK], PFNeutralObj pfallne[NNEUTRALS],
     #pragma HLS ARRAY_PARTITION variable=eToAlphas complete
     #pragma HLS ARRAY_PARTITION variable=weights complete    
 
-    for (int in = 0; in < NNEUTRALS; ++in){ 
+    for (int in = 0; in < NNEUTRALS; ++in){
+      #pragma HLS UNROLL
       eToAlphas[in] = 0; weights[in] = 0; 
     }
 
     ap_uint<17> pt2_shift[NTRACK];
     #pragma HLS ARRAY_PARTITION variable=pt2_shift complete
     for (int it = 0; it < NTRACK; ++it) {
+        #pragma HLS UNROLL
         int mypt2 = (pfch[it].hwPt*pfch[it].hwPt) >> 5;
         pt2_shift[it] = (mypt2 < 131071 ? mypt2 : 131071);
     }
 
     for (int in = 0; in < NNEUTRALS; ++in) {
+        #pragma HLS PIPELINE
         // std::cout << "Running over " << in << ": pt = " << pfallne[in].hwPt << std::endl;
         
         int sum = 0;
         for (int it = 0; it < NTRACK; ++it) {
-
-            if ((Z0 - pfch[it].hwZ0 > DZMAX) || (Z0 - pfch[it].hwZ0 < -DZMAX)) continue; // if track is PV
-            int dr2 = dr2_int(pfch[it].hwEta, pfch[it].hwPhi, pfallne[in].hwEta, pfallne[in].hwPhi); // if dr is inside puppi cone
+            auto pfch_reg = HLS_REG(pfch[it]);
+            if ((Z0 - pfch_reg.hwZ0 > DZMAX) || (Z0 - pfch_reg.hwZ0 < -DZMAX)) continue; // if track is PV
+            int dr2 = dr2_int(pfch_reg.hwEta, pfch_reg.hwPhi, pfallne[in].hwEta, pfallne[in].hwPhi); // if dr is inside puppi cone
             if (dr2 < DR2MAX) {
-            // std::cout << "(real) Looking at ch " << it << " with pt = " << pfch[it].hwPt 
+            // std::cout << "(real) Looking at ch " << it << " with pt = " << pfch_reg.hwPt 
 	    // 	      << ", dr2 = " << dr2 << ", DR2MAX = "  << DR2MAX << std::endl;
 
                 ap_uint<9> dr2short = dr2 >> 5; // why?
@@ -91,6 +94,7 @@ void simple_puppi_hw(PFChargedObj pfch[NTRACK], PFNeutralObj pfallne[NNEUTRALS],
     }
 
     for (int in = 0; in < NNEUTRALS; ++in) {
+        #pragma HLS UNROLL
         if (eToAlphas[in] < PUPPI_TABLE_SIZE) {
             if (eToAlphas[in] <= 0){
                 weights[in] = 0; // < e^10 where that is the median
@@ -121,6 +125,7 @@ void simple_puppi_hw_output(PFChargedObj pfch[NTRACK], PFNeutralObj pfallne_in[N
     PFNeutralObj pfallne[NNEUTRALS];
     #pragma HLS ARRAY_PARTITION variable=pfallne complete
     for (unsigned int i = 0; i < NNEUTRALS; i++) {
+        #pragma HLS UNROLL
         pfallne[i] = pfallne_in[i];
     }
 
